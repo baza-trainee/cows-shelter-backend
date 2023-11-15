95093eb5650133539f2699f6cb1de19250bcc6bd
cold <kholodihor@gmail.com>
2023-11-15 15:08:45 +0200

    feat: add module imports

--- src/gallery/gallery.module.ts
+++ src/gallery/gallery.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GalleryService } from './gallery.service';
 import { GalleryController } from './gallery.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Gallery } from './entities/gallery.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Gallery])],
   controllers: [GalleryController],
   providers: [GalleryService],
 })
