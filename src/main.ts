945168910e3b46571e00db9742064dc6db870623
cold <kholodihor@gmail.com>
2023-12-01 09:22:40 +0200

    update app

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,17 @@ import { get } from 'http';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
-  });
+
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
   });
 
   const config = new DocumentBuilder()
